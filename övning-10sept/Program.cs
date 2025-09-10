8224bc10f4b9a7bcebce2532af62698c20f802af
Lukas <lukas.karlsson@chasacademy.se>
2025-09-10 10:44:09 +0200

    Add project files.

---
+++ övning-10sept/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace övning_10sept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
